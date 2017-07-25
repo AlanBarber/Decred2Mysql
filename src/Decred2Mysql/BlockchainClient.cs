2656bd959f4990319de1e3d085a65687f031e98b
Alan Barber <alan@alanbarber.com>
2017-07-24 21:00:44 -0400

    Adding app

---
+++ src/Decred2Mysql/BlockchainClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Decred2Mysql
+{
+    public class BlockchainClient
+    {
+        public Uri ApiUri { get; set; }
+
+        public BlockchainClient(Uri apiUri)
+        {
+            ApiUri = apiUri ?? throw new ArgumentNullException(nameof(apiUri));
+        }
+
+        public Block GetBlock(string blockHash)
+        {
+            var query = new Uri(ApiUri, $"/api/block/{blockHash}");
+            var results = QueryApi(query);
+            return JsonConvert.DeserializeObject<Block>(results);
+        }
+
+        public Block GetBlock(int blockNum)
+        {
+            // Get hash for blockNum
+            var query = new Uri(ApiUri, $"/api/block-index/{blockNum}");
+            var results = QueryApi(query);
+            var hash = JsonConvert.DeserializeObject<BlockIndex>(results);
+            // Get block of hash
+            return GetBlock(hash.blockHash);
+        }
+
+        public BlockTransactions GetBlockTransactions(string blockHash, int pageNum)
+        {
+            var query = new Uri(ApiUri, $"/api/txs?block={blockHash}&pageNum={pageNum}");
+            var results = QueryApi(query);
+            return JsonConvert.DeserializeObject<BlockTransactions>(results);
+        }
+
+        private string QueryApi(Uri query)
+        {
+            var httpClient = new HttpClient();
+            var result = httpClient.GetStringAsync(query).Result;
+
+            return result;
+        }
+    }
+}
---
+++ src/Decred2Mysql/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Decred2Mysql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting block query...");
+
+            var blockchainClient = new BlockchainClient(new Uri("https://mainnet.decred.org/"));
+            var currentBlock = blockchainClient.GetBlock(0);
+
+            while (currentBlock?.nextblockhash != null)
+            {
+                var blockTransactions = blockchainClient.GetBlockTransactions(currentBlock.hash, 0);
+                Console.WriteLine($"Block #{currentBlock.height} - Hash: {currentBlock.hash} - Tx Count: {currentBlock.tx.Count} - API Pages: {blockTransactions.pagesTotal}");
+
+                // write data to sql here!
+
+                currentBlock = blockchainClient.GetBlock(currentBlock.nextblockhash);
+            }
+
+            #if DEBUG
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+            #endif
+        }
+
+        
+    }
+}
